db13e3db9a033a331c05086e224fb7e63f80be66
Nikita Bayev <nikita@bayev.kz>
2024-07-25 12:55:26 +0500

    fix: add optional proxy for whois

--- whois.js
+++ whois.js
@@ -4,74 +4,67 @@ const parser = require('parse-whois');
 // For CLI
 const domain = process.argv.slice(2)[0];
 
-const findFieldByAttr = (data, field) =>
-  data.find((item) => item.attribute.startsWith(field)) || {
-    value: '',
-  };
+const findFieldByAttr = (data, field) => data.find((item) => item.attribute.startsWith(field)) || {
+  value: '',
+};
 
-const findFieldsByAttrs = (data, fields) =>
-  fields.map((field) => findFieldByAttr(data, field));
+const findFieldsByAttrs = (data, fields) => fields.map((field) => findFieldByAttr(data, field));
 
 const whoisAndParse = (
   domainToParse,
   returnFull = false,
-  proxyParams = {
-    host: undefined,
-    port: undefined,
-  }
-) =>
-  new Promise((resolve, reject) => {
-    whois.lookup(
-      domainToParse,
-      {
-        proxy: {
-          host: '74.119.144.60',
-          port: 4145,
-          type: 5,
-        },
-      },
-      (err, data) => {
-        if (err) {
-          console.error('🚀 ~ whois.lookup ~ err:', err);
-        }
-        if (!data) {
-          return reject(new Error('Whois is not available'));
-        }
-        const whoisData = parser.parseWhoIsData(data);
-
-        if (returnFull) {
-          return resolve(data);
-        }
-
-        const [
-          orgName,
-          clientName,
-          clientPhoneNumber,
-          clientEmail,
-          clientAddress,
-        ] = findFieldsByAttrs(whoisData, [
-          'Organization Name',
-          'Name',
-          'Phone Number',
-          'Email Address',
-          'Street Address',
-        ]);
-
-        const parsedData = {
-          orgName: orgName.value || 'Не указано',
-          clientName: clientName.value || 'Не указано',
-          clientPhoneNumber: clientPhoneNumber.value || 'Не указан',
-          clientEmail: clientEmail.value || 'Не указан',
-          clientAddress: clientAddress.value || 'Не указан',
-        };
-
-        return resolve({
-          whoisData,
-          parsedData,
-        });
+) => new Promise((resolve, reject) => {
+  whois.lookup(
+    domainToParse,
+    {
+      proxy: process.env.WHOIS_PROXY_URL ? {
+        host: process.env.WHOIS_PROXY_URL,
+        port: process.env.WHOIS_PROXY_PORT,
+        type: 5,
+      } : null,
+    },
+    (err, data) => {
+      if (err) {
+        console.error('🚀 ~ whois.lookup ~ err:', err);
       }
-    );
-  });
+      if (!data) {
+        return reject(new Error('Whois is not available'));
+      }
+      const whoisData = parser.parseWhoIsData(data);
+
+      if (returnFull) {
+        return resolve(data);
+      }
+
+      const [
+        orgName,
+        clientName,
+        clientPhoneNumber,
+        clientEmail,
+        clientAddress,
+      ] = findFieldsByAttrs(whoisData, [
+        'Organization Name',
+        'Name',
+        'Phone Number',
+        'Email Address',
+        'Street Address',
+      ]);
+
+      const parsedData = {
+        orgName: orgName.value || 'Не указано',
+        clientName: clientName.value || 'Не указано',
+        clientPhoneNumber: clientPhoneNumber.value || 'Не указан',
+        clientEmail: clientEmail.value || 'Не указан',
+        clientAddress: clientAddress.value || 'Не указан',
+      };
+
+      return resolve({
+        whoisData,
+        parsedData,
+      });
+    },
+  );
+});
 
 if (domain) {
   whoisAndParse(domain)
