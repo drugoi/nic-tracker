3f591c808aa69fb061cc2205240bca6939a2978a
Nikita Bayev <nikita@bayev.kz>
2024-09-13 14:30:00 +0500

    feat: clean clientName from iin

--- helpers.js
+++ helpers.js
@@ -1,3 +1,7 @@
+// Example of personal data: 000924050665 (12 symbols of IIN)
+
+const cleanFromPersonalData = (text) => text.replace(/\d{12}/g, '[REDACTED]');
+
 const prepareDomainMessage = ({
   domain, orgName, clientName, clientEmail, clientAddress,
 }) => {
@@ -5,7 +9,7 @@ const prepareDomainMessage = ({
 
   if (orgName) {
     message += `*Домен:* ${domain} — [Whois](https://nic.kz/cgi-bin/whois?query=${domain})
-\n*Организация:* ${orgName}\n*Клиент:* ${clientName}\n*Email:* ${clientEmail}\n*Адрес*: ${clientAddress}\n\n`;
+\n*Организация:* ${orgName}\n*Клиент:* ${cleanFromPersonalData(clientName)}\n*Email:* ${clientEmail}\n*Адрес*: ${clientAddress}\n\n`;
   } else {
     message += `*Домен:* ${domain} — [Whois](https://nic.kz/cgi-bin/whois?query=${domain})\n\n`;
   }
